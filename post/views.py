98e3bf1bbc5e35e60a554900ef285d81c6d5046f
FloKots <florencekotohoyoh@gmail.com>
2022-06-08 07:11:27 +0300

    create new post view function

--- post/views.py
+++ post/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
-from .models import Tag, Stream, Follow, Post
+from turtle import title
+from django.shortcuts import redirect, render
+
 from django.contrib.auth.decorators import login_required
 
+from .forms import NewPostForm
+from .models import Tag, Stream, Follow, Post
 
 def index(request):
     user = request.user
@@ -13,3 +16,35 @@ def index(request):
     post_items = Post.objects.filter(id__in=group_ids).all().order_by('-posted')
     
     return render(request, 'index.html', {'post_items': post_items})
+
+
+
+def newPost(request):
+    user = request.user
+    tags_objs = []
+
+    if request.method == 'POST':
+        form = NewPostForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            image = form.cleaned_data('image')
+            caption = form.cleaned_data('caption')
+            tag_form = form.cleaned_data('tag')
+            tags_list = list(tag_form.spli(','))
+
+            for tag in tags_list:
+                t, created = Tag.objects.get_or_create(title=tag)
+                tags_objs.append(t)
+            
+            p, created = Post.objects.get_or_create(image=image, caption=caption, user_id=user)
+            p.tags_list.set(tags_objs)
+            p.save()
+
+            return redirect('index')
+
+
+    else:
+        form = NewPostForm()
+    
+    return render(request, 'newpost.html', {'form': form})
+
