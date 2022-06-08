e99d51e26e66843a79e54f3ce08c2509f962a457
FloKots <florencekotohoyoh@gmail.com>
2022-06-08 15:46:47 +0300

    add register descriptive text

--- users/forms.py
+++ users/forms.py
@@ -4,10 +4,12 @@ from django.contrib.auth.forms import UserCreationForm
 
 
 class UserRegisterForm(UserCreationForm):
-    email = forms.EmailField()
+    username = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Username', 'class': 'prompt srch_explore'}), max_length=50, required=True)
+    email = forms.EmailField(widget=forms.TextInput(attrs={'placeholder': 'Email', 'class': 'prompt srch_explore'}))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Enter Password', 'class': 'prompt srch_explore'}))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Confirm Password', 'class': 'prompt srch_explore'}))
+    
 
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
-
-
