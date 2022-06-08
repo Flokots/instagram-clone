df939b40d503e2f7b57854088aa1226c23e34cc9
FloKots <florencekotohoyoh@gmail.com>
2022-06-08 06:55:17 +0300

    define new post form class

---
+++ post/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from .models import Post
+
+class NewPostForm(forms.ModelForm):
+    image = forms.ImageField(required=True)
+    caption = forms.CharField(widget=forms.TextInput(attrs={'class': 'input', 'placeholder': 'Caption'}), required=True)
+    tag = forms.CharField(widget=forms.TextInput(attrs={'class': 'input', 'placeholder': 'Tags | Seperate with comma'}), required=True)
+
+    class Meta:
+        model = Post
+        field = ['image', 'caption', 'tag']
