f3295bf8ac57dc4abcee087ff13e44774f265190
FloKots <florencekotohoyoh@gmail.com>
2022-06-08 06:12:38 +0300

    display user's posts

--- post/views.py
+++ post/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from .models import Tag, Stream, Follow, Post
+from django.contrib.auth.decorators import login_required
 
 
 def index(request):
-    return render(request, 'index.html')
+    user = request.user
+    posts = Stream.objects.filter(user=user)
+    group_ids = []
+    for post in posts:
+        group_ids.append(post.post_id)
+        
+    post_items = Post.objects.filter(id__in=group_ids).all().order_by('-posted')
+    
+    return render(request, 'index.html', {'post_items': post_items})
