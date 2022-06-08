b836c00ceae74eb763782369816c754bcffce098
FloKots <florencekotohoyoh@gmail.com>
2022-06-08 12:49:28 +0300

    add like url pattern

--- post/urls.py
+++ post/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('newpost/', views.newPost, name='newpost'),
     path('<uuid:post_id>/', views.postDetail, name='post-details'),
     path('tag/<slug:tag_slug>/', views.tags, name='tags'),
+    path('<uuid: post_id>/like', views.like, name='like'),
 ]
