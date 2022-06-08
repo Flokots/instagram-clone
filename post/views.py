ff130272d30618ac87fe05e9718b80d99f491837
FloKots <florencekotohoyoh@gmail.com>
2022-06-08 13:02:03 +0300

    add like url to like button

--- post/views.py
+++ post/views.py
@@ -66,18 +66,18 @@ def tags(request, tag_slug):
 
 def like(request, post_id):
     user = request.user
-    post = Post.objects.get(post_id=post_id)
+    post = Post.objects.get(id=post_id)
     current_likes = post.likes
     liked = Likes.objects.filter(user=user, post=post).count()
 
     if not liked:
-        liked = Likes.objects.create(user=user, post=post)
+        Likes.objects.create(user=user, post=post)
         current_likes = current_likes + 1
     else:
-        liked = Likes.objects.filter(user=user, post=post).delete()
+        Likes.objects.filter(user=user, post=post).delete()
         current_likes = current_likes - 1
-
-        post.likes = current_likes
-        post.save()
-
-        return HttpResponseRedirect(reverse('post-details', args=[post_id]))
+        
+    post.likes = current_likes
+    post.save()
+    
+    return HttpResponseRedirect(reverse('post-details', args=[post_id]))
