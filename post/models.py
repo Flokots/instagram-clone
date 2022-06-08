71278d198640090a77b13e9541526fc0f9ebb0df
FloKots <florencekotohoyoh@gmail.com>
2022-06-08 04:31:20 +0300

    define post class model

--- post/models.py
+++ post/models.py
@@ -1,3 +1,4 @@
+from contextlib import nullcontext
 import uuid
 from django.db import models
 from django.contrib.auth.models import User
@@ -28,3 +29,21 @@ class Tag(models.Model):
         if not self.slug:
             self.slug = slugify(self.title)
         return super().save(*args, **kwargs)
+
+
+class Post(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=75, verbose_name='Name')
+    image = models.ImageField(upload_to=user_directory_path, verbose_name='Image', null=True)
+    caption = models.CharField(max_length=10000, verbose_name='Caption')
+    posted = models.DateTimeField(auto_now_add=True)
+    tag = models.ManyToManyField(Tag, related_name='tags')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    likes = models.IntegerField(default=0)
+
+    def get_absolute_url(self):
+        return reverse('post_details', args=[str(self.id)])
+
+    
+    def __str__(self):
+        return self.name
