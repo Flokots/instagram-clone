a67724fb88c5e5a729c0d62b1edfcc07f0d174fc
FloKots <florencekotohoyoh@gmail.com>
2022-06-08 08:04:11 +0300

    add post detail url

--- post/urls.py
+++ post/urls.py
@@ -4,4 +4,5 @@ from post import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('newpost/', views.newPost, name='newpost'),
+    path('<uuid:post_id>/', views.postDetail, name='post-detail'),
 ]
